f7379ca10b5c9af73fcd841347a416801b9f5568
Sil <57399085+SilTada@users.noreply.github.com>
2025-01-06 19:42:59 +0100

    adds .env, adds beat information, add command templates

--- main.js
+++ main.js
@@ -1,116 +1,77 @@
+require('dotenv').config();
 const AbletonLink = require('abletonlink');
 const { log } = require('console');
-const osc = require('osc');
-const readline = require('readline');
+const osc = require('node-osc');
 
-// Default values
-let speedmasterId = 1;
-let receiverIpAddress = "127.0.0.1";
-let receiverPort = 8000;
+// Load values from .env file
+// Network
+let ipAddress = process.env.IP_ADDRESS || '127.0.0.1';
+let port = process.env.PORT || 8000;
+// BPM Changes
+let send_BPM = process.env.SEND_BPM || true;
+let BPM_command = process.env.BPM_COMMAND || 'Master 3.1 At BPM {bpm}';
+// Beat changes
+let send_phase = process.env.SEND_PHASE || true;
+let phase_command = process.env.PHASE_COMMAND || 'Go Macro "BPM BEAT {phase}"';
+// Other
+let logging = process.env.LOGGIN || false;
+let interval = process.env.INTERVAL || 10;
+
+log('Starting Ableton Link to OSC bridge...\n');
+log('Configuration:');
+log('\n--------------- Network ---------------');
+log('IP address:              ' + ipAddress);
+log('Port:                    ' + port);
+log('\n------------- BPM changes -------------');
+log('Send changes:            ' + send_BPM);
+log('Command:                 ' + BPM_command);
+log('\n-------- Phase (beat) changes ---------');
+log('Send changes:            ' + send_phase);
+log('Command:                 ' + phase_command);
+log('\n---------------- Other ----------------');
+log('Refresh rate:            ' + interval);
+log('Logging:                 ' + logging);
+log('\n')
 
 // Create a new Ableton Link instance
 const link = new AbletonLink();
 
-// Create an OSC UDP Port
-let udpPort = new osc.UDPPort({
-    localAddress: "127.0.0.1",
-    localPort: 8001,
-    remoteAddress: receiverIpAddress,
-    remotePort: receiverPort
-});
+// Create an OSC client
+let oscClient = new osc.Client(ipAddress, port);
 
-// Open the OSC UDP Port
-udpPort.open();
-
-// Function to send BPM over OSC
-function sendBPM(masterId, bpm) {
-    udpPort.send({
-        address: "/cmd",
-        args: [
-            {
-                type: "s",
-                value: "Master 3." + masterId + " At BPM " + bpm
-            }
-        ]
-    });
+function replacePlaceholders(template, replacements) {
+    return template.replace(
+        /{(\w+)}/g, 
+        (placeholderWithDelimiters, placeholderWithoutDelimiters) =>
+            replacements.hasOwnProperty(placeholderWithoutDelimiters) ? 
+                replacements[placeholderWithoutDelimiters] : placeholderWithDelimiters
+    );
 }
 
-// Setup readline interface for user input
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-// Function to prompt user for input
-function promptUser() {
-    rl.question('Enter command (or help): ', (command) => {
-        const [action, value] = command.split(' ');
-
-        switch (action) {
-            case 'set':
-                if (value === 'id') {
-                    rl.question('Enter new speedmaster ID: ', (id) => {
-                        speedmasterId = parseInt(id, 10);
-                        console.log(`Speedmaster ID set to ${speedmasterId}`);
-                        promptUser();
-                    });
-                } else if (value === 'ip') {
-                    rl.question('Enter new receiver IP address: ', (ip) => {
-                        receiverIpAddress = ip;
-                        udpPort.options.remoteAddress = receiverIpAddress;
-                        console.log(`Receiver IP address set to ${receiverIpAddress}`);
-                        promptUser();
-                    });
-                } else if (value === 'port') {
-                    rl.question('Enter new receiver port: ', (port) => {
-                        receiverPort = parseInt(port, 10);
-                        udpPort.options.remotePort = receiverPort;
-                        console.log(`Receiver port set to ${receiverPort}`);
-                        promptUser();
-                    });
-                } else {
-                    console.log('Invalid command');
-                    promptUser();
-                }
-                break;
-            case 'start':
-                console.log('Speedmaster: ' + speedmasterId);
-                console.log('Sending OCS to: ' + receiverIpAddress + ":" + receiverPort);
-                // Listen for changes in Ableton Link
-                link.startUpdate(500, (beat, phase, bpm) => {
-                    sendBPM(speedmasterId, bpm);
-                });
-                console.log('Listening and sending...');
-                promptUser();
-                break;
-            case 'stop':
-                link.stopUpdate();
-                console.log('Stopped listening and sending.');
-                promptUser();
-                break;
-            case 'exit':
-                link.stopUpdate();
-                console.log('Stopped listening and sending.');
-                rl.close();
-                process.exit(0);
-                break;
-            case 'help':
-                console.log('Commands:');
-                console.log('set id - Set speedmaster ID');
-                console.log('set ip - Set receiver IP address');
-                console.log('set port - Set receiver port');
-                console.log('start - Start listening and sending BPM');
-                console.log('stop - Stop listening and sending BPM');
-                console.log('exit - Exit the program');
-                promptUser();
-                break;
-            default:
-                console.log('Invalid command');
-                promptUser();
-                break;
+// Function to send command over OSC
+function sendOSCCommand(command, variables) {
+    let cmd = replacePlaceholders(command, variables);
+    oscClient.send('/cmd', cmd, () => {
+        if(logging) {
+            log('GrandMA command sent: ' + cmd);
         }
     });
 }
 
-// Start prompting user for input
-promptUser();
+let lastPhase = 0;
+let lastBPM = 0;
+// Listen for changes in Ableton Link
+link.startUpdate(interval, (beat, phase, bpm) => {
+    if(bpm !== lastBPM && send_BPM) {
+        sendOSCCommand(BPM_command, { bpm: bpm });
+    }
+    lastBPM = bpm;
+    phase = Math.floor(phase);
+    if(phase !== lastPhase && send_phase) {
+        sendOSCCommand(phase_command, { phase: parseInt(phase) + 1 });
+    }
+    lastPhase =  phase;
+    
+});
+
+log('Listening to Ableton Link...')
--- readme.md
+++ readme.md
@@ -1,18 +1,18 @@
 # AbletonLink-GMA3
 
-AbletonLink-GMA3 is a project that integrates Ableton Link with the GrandMA3 lighting console, allowing for synchronized tempo information between Ableton Live and GrandMA3.
+AbletonLink-GMA3 is a project that integrates Ableton Link with the GrandMA3 lighting console, allowing for synchronized tempo and beat information between Ableton Live and GrandMA3.
 
 ## Features
 
-- Synchronize tempo information between Ableton Live and GrandMA3
+- Synchronize tempo and beat information between Ableton Live and GrandMA3
 - Easy setup and configuration
 - Real-time tempo adjustments
 
 ## Requirements
 
-- Ableton Link
-- GrandMA3 lighting console or software
-- Node.js
+- Ableton Link enabled software (such as Ableton Live or Pulse)
+- GrandMA3 lighting console or onPC software
+- Node.js installed on your system
 
 ## Installation
 
@@ -29,21 +29,55 @@ AbletonLink-GMA3 is a project that integrates Ableton Link with the GrandMA3 lig
     npm install
     ```
 
-## Usage
+## Configuration of the Script
+
+All settings have hardcoded default values, but can be modified by copying and renaming the `.env.default` file to `.env` in the root directory. All settings mentioned in the default `.env` file can be customized.
+
+### Steps to Configure
+
+1. Copy the `.env.default` file:
+    ```sh
+    cp .env.default .env
+    ```
+2. Open the newly created `.env` file in a text editor.
+3. Modify the settings as needed. Refer to the table below for available settings and their descriptions.
+
+### Available Settings
+
+| Setting             | Description                                      | Default value                     | Available placeholders    |
+|---------------------|--------------------------------------------------|-----------------------------------|---------------------------|
+| **Network**         |                                                  |                                   |                           |
+| `IP_ADDRESS`        | IP address of the GrandMA console or onPC software.                     | 127.0.0.1                         |                           |
+| `PORT`              | Port number for the OSC messages. This should match the port configured in the GrandMA console or onPC software.                | 8000                              |                           |
+| **BPM Changes**     |                                                  |                                   |                           |
+| `SEND_BPM`          | Enable/disable sending BPM changes.               | true                              |                           |
+| `BPM_COMMAND`       | Command template that will be sent to the GrandMA when a change in BPM value is present.                 | Master 3.1 At BPM {bpm}        | `{bpm}`           |
+| **Phase (beat) changes** |                                             |                                   |                           |
+| `SEND_PHASE`        | Enable/disable sending phase (beat) changes.      | true                              |                           |
+| `PHASE_COMMAND`     | Command template that will be sent to the GrandMA when a change in phase (beat) is present.        | Go Macro "BPM BEAT {phase}"       | `{phase}`                 |
+| **Other**           |                                                  |                                   |                           |
+| `LOGGING`           | Enable/disable logging of sent OSC commands.          | false                             |                           |
+| `INTERVAL`          | Interval for checking Ableton Link values (in milliseconds)  | 10                                |                           |
+
+## Configuration of the GrandMA console or onPC software
+
+1. Open the settings menu and go to `In & Out`.
+2. Go to the `OSC` tab.
+3. Set the `Preferred IP` to the IP range the desk and the script are connected through.
+4. Set the `Interface` to the one that is connecting the script and the console or software.
+5. Make sure that "Enable Input" is ticked.
+6. Insert a new OSCData line:
+    - Fill in the IP of the device from where the script is running as the Destination IP (default is 127.0.0.1), as well as the port number as the port that is specified in the `.env` file (default is 8000).
+    - Make sure `Receive Command` is set to `Yes`.
+
+## Usage of the script
 
 1. Start any Ableton Link enabled software.
 2. Run the AbletonLink-GMA3 script:
     ```sh
     npm start
     ```
-3. If needed, change the ip address and port to where the script should send the OSC commands. As well as which speedmaster should be updated.
-4. Configure the GrandMA3 console or software to receive OSC commands from the computer the script is running from:
-    - Open the GrandMA3 software or console.
-    - Navigate to the "Setup" menu.
-    - Select "In & Out" and go to the OSC tab.
-    - Insert a new OSCData line.
-    - Set the OSC destination IP as well as the port to match the port specified in the AbletonLink-GMA3 script.
-    - Enable OSC input.
+3. Commands should be running from the script to the GrandMA.
 
 ## Contact
 
